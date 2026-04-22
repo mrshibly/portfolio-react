4709e90da839a2484bd3915ecc6c483c53bad2e2
mrshibly <mahmudurrahman858@gmail.com>
2026-04-22 23:28:04 +0600

    Fix Express 5 catch-all route syntax

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // Catch-all route to serve the React app
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
